63514aab04febeed2bfe29f6fd6a44a2d146fe0c
anuj <anujgupta596@gmail.com>
2022-03-23 15:43:41 +0530

    Final changes

---
+++ src/app/pipes/filter.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'search'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(user: any, searchValue:string) {
+    if(user.length===0 || searchValue===''){
+      return user;
+    }
+
+    return user.filter((lock:any) => {
+      return (lock.name.toUpperCase().match(searchValue.toUpperCase()));
+    });
+    }
+   
+   
+  }
+
+
